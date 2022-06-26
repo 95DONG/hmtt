21f2f869e7396f65232c161ca9489f20c7b783ed
DONG <969349879@qq.com>
2022-06-26 11:44:19 +0800

    路由配置完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Layout from '@/views/Layout'
+const Home = () => import('@/views/Home')
+const Question = () => import('@/views/Question')
+const My = () => import('@/views/My')
+const Video = () => import('@/views/Video')
+const Login = () => import('@/views/Login')
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      { path: 'my', component: My },
+      { path: 'question', component: Question },
+      { path: 'video', component: Video },
+      { path: 'home', component: Home }
+    ]
+  },
+  {
+    path: '/login',
+    component: Login
+  }
 ]
 
 const router = new VueRouter({
